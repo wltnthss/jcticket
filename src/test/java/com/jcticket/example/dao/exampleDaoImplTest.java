772bc89fc608033c616f90e756023c8fd7654fef
wook219 <wlsnr19731@gmail.com>
2024-01-29 23:43:53 +0900

    테스트 찢겼다

---
+++ src/test/java/com/jcticket/example/dao/exampleDaoImplTest.java
@@ -0,0 +1,47 @@
+package com.jcticket.example.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * packageName    : com.jcticket.example.dao
+ * fileName       : exampleDaoImplTest
+ * author         : jinwook Song
+ * date           : 2024-01-29
+ * description    : 테스트케이스를 테스트
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-01-29        jinwook Song       최초 생성
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class exampleDaoImplTest {
+@Autowired
+exampleDao exampleDao;
+
+    @Test
+    public void test1() {
+        System.out.println("exampleDao = " + exampleDao);
+        assertTrue(exampleDao!=null);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        System.out.println("exampleDao.test() = " + exampleDao.test());
+        Date now = new Date();
+        System.out.println(now);
+//        assertTrue(exampleDao.test()==ld);
+    }
+
+}
